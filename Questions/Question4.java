a2730c1b62aac7f4943f9deee1a1b61b295b3753
Kamal Sharma <kamaldgrt@gmail.com>
2020-01-22 19:23:44 +0530

    Update Question4.java
    Still some work is there left.
    will be done soon..

--- Questions/Question4.java
+++ Questions/Question4.java
@@ -5,13 +5,24 @@ public class Question3
 {
   public static void main(String[] args)
   {
+      Vehicle Cycle = new Vehicle();
+      Vehicle Car = new Vehicle();
+      Vehicle Auto = new Vehicle();
 
+      Cycle.read();
+      Car.read();
+      Auto.read();
+
+      System.out.println("\n\n Displaying the vehicle details : \n\n");
+
+      Cycle.display();
+      Car.display();
   }
 }
 
 class Vehicle
 {
-  String modelName, regNo;
+  String modelName, regNo, noOfWheels;
 
   void read()
   {
@@ -20,6 +31,14 @@ class Vehicle
     modelName = S.newString();
     System.out.println("Enter Register Number : ");
     regNo = S.newString();
-    
+    System.out.println("Enter Number of Wheels : ");
+    noOfWheels = S.newInt();
+  }
+
+  void display()
+  {
+    System.out.println(" Model Name      : " + modelName);
+    System.out.println("Register Number  : " + regNo);
+    System.out.println("Number of Wheels : " + noOfWheels);
   }
 }
