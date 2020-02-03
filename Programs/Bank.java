f80c1c586b75c0f5615dc2537cde6853904563da
Kamal Sharma <kamaldgrt@gmail.com>
2020-02-03 21:17:20 +0530

    update notes and create bank.java

---
+++ Programs/Bank.java
@@ -0,0 +1,28 @@
+class Bank
+{
+    double rateOfInterest()
+    {
+        return 0.5;
+    }
+}
+
+class Indian extends Bank
+{
+    double rateOfInterest()
+    {
+        return 0.75;
+    }
+}
+
+class HDFC extends Bank
+{
+    double rateOfInterest()
+    {
+        return 0.9;
+    }
+}
+
+class ICICI extends Bank
+{
+    
+}
