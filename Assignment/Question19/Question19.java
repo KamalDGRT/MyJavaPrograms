9d9618825a05a4cdbfb72a555161c2f3839d0029
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-26 21:29:35 +0530

    Create Question19.java

---
+++ Assignment/Question19/Question19.java
@@ -0,0 +1,6 @@
+/*
+Implement a superclass Person. Make two classes, Student and Instructor, inherit from
+Person. A person has a name and a year of birth. A student has a major, and an instructor
+has a salary. Write the class definitions, the constructors, and the methods toString for all
+classes. Supply a test program that tests these classes and methods.
+*/
