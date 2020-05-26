dd443cb241fec3327795364cdebbc742e130390a
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-26 21:24:37 +0530

    Create Question15.java

---
+++ Assignment/Question15/Question15.java
@@ -0,0 +1,22 @@
+/*  The certain instructor assigns letter grade for his course based on the following table:
+Score   Grade
+>= 90   A+
+>= 85   A
+>= 80   B+
+>= 75   B
+>= 70   C+
+>= 65   C
+>= 60   D+
+>= 55   D
+<50     F
+
+Write a class, Grader, which has an instance variable, score, an appropriate constructor and
+appropriate methods
+- a method, letterGrade() that returns the letter grade as a String.
+
+Now write a demo class to test the Grader class by reading a score from the user,
+using it to create a Grader object after validating that the value is not negative and is not
+greater then 100. Finally, call the letterGrade() method to get and print the grade. See figure
+(b) for sample run.
+
+*/
