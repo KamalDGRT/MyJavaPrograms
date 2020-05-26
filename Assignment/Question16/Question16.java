d0f859f1ca869664e8d8b3c9e92943a6c740ea44
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-26 21:26:01 +0530

    Create Question16.java

---
+++ Assignment/Question16/Question16.java
@@ -0,0 +1,5 @@
+/*
+Write a program to make use of a parameterized method inside a class. Take the following
+case: Create a class Box and define a method in this class which will return the volume of the
+box. Initialize two objects for your class and print out the volumes respectively.
+*/
