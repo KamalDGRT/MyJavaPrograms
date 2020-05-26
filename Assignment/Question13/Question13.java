9a9b184b2c37e0d3540ad9e11d77b73ee2c279cc
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-26 21:23:25 +0530

    Create Question13.java

---
+++ Assignment/Question13/Question13.java
@@ -0,0 +1,12 @@
+/*
+
+13. In a small firm employee numbers are given in serial numerical order, that is 1, 2, 3 etc. write
+a menu driven program to perform following operations: 
+a) Create a file of employee data with following information: Employee No, Name, Sex, Gross Salary. 
+b) Append the data of a new employee joining the firm.
+c) If a given employee leaves, delete his record. 
+d) If gross salary of a given employee increases, update the gross salary. 
+e) Display the record of (i) a given employee or (ii) all employees.
+
+*/
+
