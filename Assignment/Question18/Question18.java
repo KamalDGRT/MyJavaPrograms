093f21b8b932af9b172d435322db9fa81d415b70
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-26 21:28:16 +0530

    Create Question18.java

---
+++ Assignment/Question18/Question18.java
@@ -0,0 +1,15 @@
+/*
+Write a program to design a class to represent a bank account. Include the following members.
+
+Data members
+- Name of depositor
+- Account Number
+- Type of account
+- Balance account in the account
+
+Methods:- 
+- To assign initial values
+- To deposit an account
+- To withdraw an account after checking balance.
+- To display the name and balance
+*/
