33ff56806d5691cb92290b5203ce56b734dd5dbf
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-27 22:24:34 +0530

    Create Question22.java

---
+++ Assignment/Question22/Question22.java
@@ -0,0 +1,6 @@
+/*
+Write a program that calls a method that throws an exception of type ArithmeticExcepton at
+a random iteration in a for loop. Catch the exception in the method and pass the iteration
+count when the exception occurred to the calling method by using an object of an exception
+class you define.
+*/
