0a2540f186f2a813b524aeaea1e5d820832412f4
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-28 23:53:48 +0530

    Create Question31.java

---
+++ Assignment/Question31/Question31.java
@@ -0,0 +1,7 @@
+/*
+Write a Java program to create a super class Vehicle having members Company and price.
+Derive 2 different classes LightMotorVehicle (members – mileage) and HeavyMotorVehicle
+(members – capacity-in-tons). Accept the information for n vehicles and display the
+information in appropriate form. While taking data, ask the user about the type of vehicle
+first.
+*/
