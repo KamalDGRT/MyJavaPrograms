56b37212725d19c63dd0f10a243abe1152b69a2f
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-28 23:47:50 +0530

    Create Question26.java

---
+++ Assignment/Question26/Question26.java
@@ -0,0 +1,5 @@
+/*
+Write an applet that will display the following on a green background. Use the following
+dimension: Rectangle: (10, 10, 300, 150), fill color: blue Left circle: (10, 10, 150, 150), fill
+color: yellow Right circle: (159, 10, 150, 150), fill color: yellow Text: (110, 90), color: red
+*/
