1df1150c95130f0e4b4c9cffd5b35584ea68c6ef
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-28 23:45:46 +0530

    add answer code for question17

--- Assignment/Question17/Question17.java
+++ Assignment/Question17/Question17.java
@@ -22,3 +22,60 @@ class to create a Student object. Then read the scores of the student in three q
 and add each to the totalScore of the student using addQuiz() method and print the
 student object.
 */
+
+class Student{
+    private String  name;
+    private int numberOfQuizzes;
+    private int totalScore;
+    private double myAverage;
+
+    public Student(String name) {
+        this.name = name;
+        numberOfQuizzes = 0;
+        totalScore = 0;
+        myAverage = 0;
+    } 
+
+    public Student() {
+        this("");
+    }
+
+    public void addQuiz(int score)  {
+        totalScore = totalScore + score; 
+        numberOfQuizzes++;
+    }
+
+    public void showDetals() {
+        System.out.println("Name: " + name );
+        System.out.println("#Quizzes: " + numberOfQuizzes);  
+        System.out.println("Total Quiz Score: " + totalScore); 
+        System.out.println("Average Score: " + myAverage);
+    }
+
+    public void calculateAverageScore() {
+        if (numberOfQuizzes == 0) 
+            myAverage = 0.0;
+        else
+            myAverage = Math.round((double)totalScore/ (double)numberOfQuizzes);
+    }
+}
+
+class Question17 {
+    public static void main(String[] args) {        
+        Student Test = new Student("Percy Jackson");
+        Test.addQuiz(120);
+        Test.addQuiz(160);
+        Test.addQuiz(220);
+        Test.calculateAverageScore();
+        Test.showDetals();
+    }
+}
+
+/* Output after execution :
+
+Name: Percy Jackson
+#Quizzes: 3
+Total Quiz Score: 500
+Average Score: 167.0
+
+*/
