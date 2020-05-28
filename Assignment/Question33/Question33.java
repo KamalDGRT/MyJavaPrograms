c5971d86bb12607c95890bf6d7b85678b4c177f1
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-28 23:55:54 +0530

    Rename Q32 folder and create Q33

---
+++ Assignment/Question33/Question33.java
@@ -0,0 +1,8 @@
+/*
+Define a class called fruit with the following attributes:
+1. Name of the fruit.
+2. Single fruit or bunch fruit.
+3. Price.
+Define a suitable constructor and displayFruit() method that displays values of all the
+attributes. Write a program that creates 2 objects of fruit class and display their attributes.
+*/
