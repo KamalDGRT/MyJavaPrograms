178f833db3016917ec2255c2777366e9ef0ab052
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-28 23:51:23 +0530

    Create Question29.java

---
+++ Assignment/Question29/Question29.java
@@ -0,0 +1,7 @@
+/*
+Write a graphics program that draws a clock face with a time that the user enters in a text
+field. (The user must enter the time in the format hh:mm, for example 09:45. ). Hint: You
+need to find out the angles of the hour hand and the minute hand. The angle of the hour
+hand is harder; it travels 360 degree in 12 x 60 minutes. Your output must be in the
+following format:
+*/
