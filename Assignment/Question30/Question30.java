6b77cb91527abeab3ddf4ca9d8824e7825f1a728
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-28 23:52:59 +0530

    Create Question30.java

---
+++ Assignment/Question30/Question30.java
@@ -0,0 +1,7 @@
+/*
+Define a class Employee having private members – id, name, department, salary. Define
+default and parameterized constructors. Create a subclass called “Manager” with private
+member bonus. Define methods accept and display in both the classes. Create n objects of
+the Manager class and display the details of the manager having the maximum total salary
+(salary+bonus)
+*/
