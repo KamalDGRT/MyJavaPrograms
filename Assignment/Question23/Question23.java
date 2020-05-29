822231ce6a0d92b5d1855b63a73a7a0011c4af0f
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-29 16:48:19 +0530

    add code for question 23

--- Assignment/Question23/Question23.java
+++ Assignment/Question23/Question23.java
@@ -1,3 +1,32 @@
 /*
 Write a program that will count the number of character in a file.
 */
+
+import java.io.*; 
+
+class Question23 { 
+    public static void main(String[] args) throws IOException  { 
+        File file = new File("data.txt"); 
+        FileInputStream fileStream = new FileInputStream(file); 
+        InputStreamReader input = new InputStreamReader(fileStream); 
+        BufferedReader reader = new BufferedReader(input); 
+
+        String line; 
+
+        // Initializing counters 
+        int characterCount = 0; 
+
+        // Reading line by line from the file until a null is returned 
+        while((line = reader.readLine()) != null)  { 
+            if(!line.equals(""))             
+                characterCount += line.length(); 
+        } 
+        System.out.println("Total number of characters = " + characterCount);
+    } 
+} 
+
+/* Outptut after execution 
+
+Total number of characters = 141
+
+*/
