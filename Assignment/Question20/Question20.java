227d7cb3b0f4ab5a9686c5c0e831cd7ef29da6b5
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-29 16:12:12 +0530

    Add code for Question20

--- Assignment/Question20/Question20.java
+++ Assignment/Question20/Question20.java
@@ -2,3 +2,34 @@
 Write a program where interface can be used to support multiple inheritances. Develop a
 standalone Java program for this.
 */
+
+interface Sing {
+    default void singRock() {
+        System.out.println("I am singing rock.");
+    }
+}
+
+interface Dance {
+    default void danceSlow(){
+        System.out.println("I am dancing slow!");
+    }
+}
+
+class Human implements Sing, Dance {
+
+}
+
+class Question20 {
+    public static void main(String[] args) {
+        Human h = new Human();
+        h.singRock();
+        h.danceSlow();
+    }
+}
+
+/* Output after execution :
+
+I am singing rock.
+I am dancing slow!
+
+*/
